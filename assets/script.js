f59752130970ad2aee7790afcc271cbd6b28fba2
Matt <mkeeney4202@gmail.com>
2024-06-05 14:53:33 -0400

    Fetched to the weather API and got todays data

--- assets/script.js
+++ assets/script.js
@@ -3,7 +3,25 @@ const apiKey = "99e151147e4dcc5b60bb5dd86ee0cf78"
 const searchButton = document.querySelector(".search-button")
 
 function searchPlace(cityName){
-console.log(cityName)
+fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=imperial&appid=${apiKey}`
+    ).then(function(response){
+        return response.json()
+    }).then(function(data){
+        console.log(data)
+        const weatherContainer = document.querySelector(".today-weather")
+        const todaysWeather = `
+        <div>
+        <h3>city: ${data.name}</h3>
+        <p>Temp: ${data.main.temp}</p>
+        <p>Humidity: ${data.main.humidity}</p> 
+        <p>Wind Speed: ${data.wind.speed}</p>
+        </div>
+        
+        `;
+
+        weatherContainer.innerHTML = todaysWeather
+    })
 
 }
 
@@ -16,3 +34,4 @@ searchPlace(cityName)
 
 
 })
+ 
